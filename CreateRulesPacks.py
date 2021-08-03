0b5e1b79fa40078ad0829b38fd47d519bed83244
s.milyutinskiy <smilyutinskiy@simple.ru>
2021-08-03 13:16:20 +0300

    Добавлена автозагрузка правил в тестовые базы

--- CreateRulesPacks.py
+++ CreateRulesPacks.py
@@ -6,18 +6,7 @@ import re
 import sys
 import json
 import shutil
-
-with open('settings.json', encoding="utf8") as settigs_file:
-    settings = json.load(settigs_file)
-    first_path = settings['rules']['firstPath']    # 'DocFlow/Правила конвертации DF2ERPBF/'
-    second_path = settings['rules']['secondPath']  # 'ERP BF/Правила конвертации ERPBF2DF/'
-    first_zip = settings['rules']['firstZip']      # 'Правила обмена DF2ERPBF.zip'
-    second_zip = settings['rules']['secondZip']    # 'Правила обмена ERPBF2DF.zip'
-    synckCorrAndExch = settings['rules']['synckCorrAndExch'] # 'флаг синхронизации файлов CorrespondentExchangeRules.xml и CorrespondentExchangeRules.xml'
-
-includes = ['*.xml']
-includes = r'|'.join([fnmatch.translate(x) for x in includes])
-
+import subprocess
 
 def zipdir(path, ziph):
     # ziph is zipfile handle
@@ -36,6 +25,24 @@ def create_zip(path, name):
 
 
 if __name__ == '__main__':
+
+    with open('settings.json', encoding="utf8") as settigs_file:
+        settings = json.load(settigs_file)
+        first_path = settings['rules']['firstPath']    # 'DocFlow/Правила конвертации DF2ERPBF/'
+        second_path = settings['rules']['secondPath']  # 'ERP BF/Правила конвертации ERPBF2DF/'
+        first_zip = settings['rules']['firstZip']      # 'Правила обмена DF2ERPBF.zip'
+        second_zip = settings['rules']['secondZip']    # 'Правила обмена ERPBF2DF.zip'
+
+        first_base_server = settings['test']['firstBaseServer']
+        second_base_server = settings['test']['secondBaseServer']
+        first_base_name = settings['test']['firstBaseName']
+        second_base_name = settings['test']['secondBaseName']
+        exchange_name = settings['test']['exchangeName']
+        set_rules_processor = settings['test']['setRulesProcessorPath']
+
+    includes = ['*.xml']
+    includes = r'|'.join([fnmatch.translate(x) for x in includes])
+
     # Строим полные пути от папки запуска
     start_path = os.path.dirname(sys.argv[0])
     first_path = os.path.join(start_path, first_path)
@@ -45,10 +52,19 @@ if __name__ == '__main__':
     second_zip = os.path.join(start_path, second_zip)
 
     # Синхронизируем правила выгрузки с правилами корреспондирующей базы
-    # ExchangeRules.xml CorrespondentExchangeRules.xml
-    if synckCorrAndExch == True:
-        shutil.copy(os.path.join(first_path, r'ExchangeRules.xml'), os.path.join(second_path, r'CorrespondentExchangeRules.xml'))
-        shutil.copy(os.path.join(second_path, r'ExchangeRules.xml'), os.path.join(first_path, r'CorrespondentExchangeRules.xml'))
+    shutil.copy(os.path.join(first_path, r'ExchangeRules.xml'), os.path.join(second_path, r'CorrespondentExchangeRules.xml'))
+    shutil.copy(os.path.join(second_path, r'ExchangeRules.xml'), os.path.join(first_path, r'CorrespondentExchangeRules.xml'))
 
     create_zip(first_path, first_zip)
     create_zip(second_path, second_zip)
+ 
+    set_rules_command = '"C:\\Program Files\\1cv8\\8.3.18.1483\\bin\\1cv8.exe" ENTERPRISE \
+        /S"{}/{}" \
+        /DisableStartUpMessages \
+        /DisableStartupDialogs \
+        /Execute"{}" \
+        /C"ИмяПланаОбмена={};ФайлПравил={}"'.replace("  ","")
+
+    subprocess.check_call(set_rules_command.format(first_base_server,first_base_name,set_rules_processor,exchange_name,first_zip), shell=True)
+    subprocess.check_call(set_rules_command.format(second_base_server,second_base_name,set_rules_processor,exchange_name,second_zip), shell=True)
+    pass
